3e081d891939509f335fb86372cfea183677ad45
mengyujackson121 <mengyujackson121@gmail.com>
2017-09-15 07:27:42 -0400

    Initial commit

---
+++ src/Is_Unique.java
@@ -0,0 +1,20 @@
+public class Is_Unique {
+    public static void main(String[] args) {
+        boolean a = is_UniqueChars("5daf");
+        System.out.print(a);
+    }
+    static boolean is_UniqueChars(String str){
+        if (str.length() > 128) return false;
+
+        boolean[] char_set = new boolean[128];
+        for (int i = 0; i < str.length();i++){
+            int val = str.charAt(i);
+            if (char_set[val]){
+                return false;
+            }
+            char_set[val] = true;
+        }
+        return true;
+    }
+}
+
---
+++ src/is_Unique2.java
@@ -0,0 +1,19 @@
+public class is_Unique2 {
+    public static void main(String[] args) {
+        boolean a = isUniqueChars("5daff");
+        System.out.print(a);
+    }
+   static boolean isUniqueChars (String str){
+
+            int checker = 0;
+            for (int i = 0; i < str.length(); i++) {
+                int val = str.charAt(i) - 'a';
+                if ((checker & (1 << val)) > 0) {
+                    return false;
+                }
+                checker |= (1 << val);
+            }
+            return true;
+        }
+}
+
