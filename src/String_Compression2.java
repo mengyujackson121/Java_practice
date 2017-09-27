2f50374cb7f1217e44e7e47cd1f4900e81396357
mengyujackson121 <mengyujackson121@gmail.com>
2017-09-27 14:58:39 -0400

    String Compression

---
+++ src/String_Compression2.java
@@ -0,0 +1,16 @@
+public class String_Compression2
+{
+    String compress(String str){
+        StringBuilder compressed = new StringBuilder();
+        int countConsecutive = 0;
+        for (int i = 0; i < str.length(); i++){
+            countConsecutive++;
+        }
+        if(i + 1 >= str.length() || str.length(i) != str.charAt(i + 1)){
+            compressed.append(str.charAt(i));
+            compressed.append(countConsecutive);
+            countConsecutive = 0;
+        }
+        return compressed.length() < str.length() ? compressed.toString() :str;
+    }
+}
