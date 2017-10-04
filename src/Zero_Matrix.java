7b89866c75000cc311ba8534c61b616351379506
mengyujackson121 <mengyujackson121@gmail.com>
2017-10-04 17:35:51 -0400

    Zero_Matrix

---
+++ src/Zero_Matrix.java
@@ -0,0 +1,34 @@
+public class Zero_Matrix {
+    void setZeros(int[][] matrix) {
+        boolean[] row = new boolean[matrix.length];
+        boolean[] column = new boolean[matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    column[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < row.length; i++) {
+            if (row[i]) nullifyRow(matrix, 1);
+        }
+        for (int j = 0; j < column.length; j++) {
+            if (column[j]) nullifyColumn(matrix, j);
+        }
+    }
+
+    void nullifyRow(int[][] matrix, int row) {
+        for (int j = 0; j < matrix[0].length; j++) {
+            matrix[row][j] = 0;
+        }
+    }
+
+    void nullifyColumn(int[][] matrix, int col) {
+        for (int i = 0; i < matrix[0].length; i++) {
+            matrix[i][col] = 0;
+        }
+    }
+}
+
